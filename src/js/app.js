82d7b85b68131bd3b3b7a4cc32abd55b79b1ad2a
Greg Hoyl <greg.hoyl@xero.com>
2015-04-21 21:00:37 +1200

    note about localstorage app cache

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@ var App = require('./components/App.react');
 
 /**
  * Save app to localstorage?
+ * Works well except for background image, as the base64 not
+ * rendering to string all that well via React.renderToString
+ *
  * http://www.tabforacause.org/blog/2015/01/29/using-reactjs-and-application-cache-fast-synced-app/
  */
 
