7996320ae925f09572176f56e00ee8b5c630f3d7
ghjunior <greg.hoyl@xero.com>
2015-04-23 22:04:22 +1200

    update analytics to use prod/dev IDs

--- src/js/utils/Analytics.js
+++ src/js/utils/Analytics.js
@@ -3,7 +3,14 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-62023248-2', 'auto');
+var analyticsID = 'UA-62023248-3';
+
+// Track under Tupiq Dev when running local
+if (chrome.runtime.getManifest().oauth2.client_id === '1022186080812-fpgbc34rns8pdtnkefat2jmac9fatdt2.apps.googleusercontent.com') {
+	analyticsID = 'UA-62023248-2'
+}
+
+ga('create', analyticsID, 'auto');
 ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
 ga('require', 'displayfeatures');
 
