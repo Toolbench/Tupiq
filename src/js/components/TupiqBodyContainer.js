831172679e17f90b198a5c33ab127d7a65841f1a
ghjunior <ghjunior@gmail.com>
2015-05-12 19:17:06 +1200

    remove commented

--- src/js/components/TupiqBodyContainer.js
+++ src/js/components/TupiqBodyContainer.js
@@ -6,47 +6,14 @@ var TupiqTools = require('../utils/TupiqTools');
 var Analytics = require('../utils/Analytics');
 
 /**
- * Stores
- */
-//var CalendarStore = require('../stores/CalendarStore');
-
-/**
- * Actions
- */
-//var CalendarActions = require('../actions/CalendarActions');
-
-/**
  * Pure component
  */
 var TupiqBody = require('./TupiqBody');
 
 /**
- * Private
- */
-function getStateFromStores() {
-  return {};
-}
-
-/**
  * TupiqBodyContainer
  */
 var TupiqBodyContainer = React.createClass({
-  getInitialState: function() {
-    return getStateFromStores();
-  },
-
-  componentDidMount: function() {
-    //CalendarStore.addChangeListener(this._onChange);
-  },
-
-  componentWillUnmount: function() {
-    //CalendarStore.removeChangeListener(this._onChange);
-  },
-
-  _onChange: function() {
-    this.setState(getStateFromStores());
-  },
-
   render: function() {
 		return (
 			<TupiqBody />
