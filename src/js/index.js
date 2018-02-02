e284dbe81f227fa9b88c38448988a252d8dd3702
Gregory Hoyl Jr <greg.hoyl@xero.com>
2018-02-02 15:03:17 +1300

    indexeddb stuff

--- src/js/index.js
+++ src/js/index.js
@@ -3,32 +3,62 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
+import LZString from 'lz-string';
 import logger from 'redux-logger';
 import App from './areas/app';
 import reducer from './reducers';
 import sagas from './sagas';
 
-const persistMiddleware = store => next => (action) => {
-  next(action);
-  localStorage.setItem('state', JSON.stringify(store.getState()));
-};
+/* eslint-disable */
+let transaction;
 
-const persistedState = localStorage.getItem('state');
-const serializedState = (persistedState) ? JSON.parse(persistedState) : undefined;
+const request = indexedDB.open('tupiq', 1);
 
-const sagaMiddleware = createSagaMiddleware();
+request.onupgradeneeded = event => {
+  const db = event.target.result;
 
-const store = createStore(
-  reducer,
-  serializedState,
-  applyMiddleware(sagaMiddleware, persistMiddleware, logger)
-);
+  db.createObjectStore('state');
+}
 
-sagaMiddleware.run(sagas);
+request.onsuccess = event => {
+  const db = event.target.result;
+  const objectStore = db.transaction(['state'], 'readwrite').objectStore('state');
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+  const persistMiddleware = store => next => (action) => {
+    next(action);
+
+    const objectStore = db.transaction(['state'], 'readwrite').objectStore('state');
+    const request = objectStore.put(LZString.compressToUint8Array(JSON.stringify(store.getState())), 'theState');
+
+    request.onsuccess = event => {
+      console.log('great success');
+    }
+  };
+
+  const getPersistedStateRequest = objectStore.get('theState');
+
+  getPersistedStateRequest.onsuccess = event => {
+    const persistedState = event.target.result;
+    const serializedState = (persistedState) ? JSON.parse(LZString.decompressFromUint8Array(persistedState)) : undefined;
+    const sagaMiddleware = createSagaMiddleware();
+    
+    const store = createStore(
+      reducer,
+      serializedState,
+      applyMiddleware(sagaMiddleware, persistMiddleware, logger)
+    );
+    
+    sagaMiddleware.run(sagas);
+    
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById('root')
+    );  
+  }
+
+  getPersistedStateRequest.onerror = error => {
+    //debugger;
+  }
+}
